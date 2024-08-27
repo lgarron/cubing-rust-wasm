3fb8e39808f0906a339818d67511e4c2dfada652
Lucas Garron <code@garron.net>
2024-08-27 10:57:32 +0200

    Use `getBuiltinModule(…)`.

--- script/node-esm-compat.js
+++ script/node-esm-compat.js
@@ -1,31 +1,28 @@
-import { readdir, readFile, writeFile } from "fs/promises";
-import { join } from "path";
+import { readFile, readdir, writeFile } from "node:fs/promises";
+import { join } from "node:path";
 
 const filePath = new URL("../pkg/cubing_rust_wasm.js", import.meta.url);
 
 let modified = false; // For idempotence
 
 let contents = await readFile(filePath, "utf-8");
-const lines = [`// Mangled so that bundlers don't try to inline the source.
-const node_fs_promises_mangled = "node:-fs/pr-omises";
-const node_fs_promises_unmangled = () => node_fs_promises_mangled.replace(/-/g, "");
-`];
+const lines = [];
 for (const line of contents.split("\n")) {
-  if (line.trim() === "input = fetch(input);") {
-    lines.push(`        try {
+	if (line.trim() === "input = fetch(input);") {
+		lines.push(`        try {
             input = await fetch(input);
         } catch (e) {
             if (!(e instanceof TypeError)) {
                 throw e;
             }
-            input = await (await import(node_fs_promises_unmangled())).readFile(input);
+            input = await globalThis.process.getBuiltinModule("node:fs/promises").readFile(input);
         }`);
-    modified = true;
-  } else {
-    lines.push(line);
-  }
+		modified = true;
+	} else {
+		lines.push(line);
+	}
 }
 if (modified) {
-  contents = lines.join("\n");
-  await writeFile(filePath, contents);
+	contents = lines.join("\n");
+	await writeFile(filePath, contents);
 }
